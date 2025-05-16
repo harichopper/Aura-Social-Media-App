5caef0bfb21f19758206feb980aa5c6a29f76a6a
HARIHARAN R <144225049+harichopper@users.noreply.github.com>
2025-05-16 16:22:57 +0530

    Update fetchData.js

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-const REACT_API_URL="https://aura-social-media-app.vercel.app";
+// Create a reusable Axios instance
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true
+});
+
+
 /**
  * GET request
  * @param {string} url - API endpoint
@@ -8,10 +14,10 @@ const REACT_API_URL="https://aura-social-media-app.vercel.app";
  * @returns {Promise} - Axios response promise
  */
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
-        headers: { Authorization: token }
-    });
-    return res;
+  const res = await API.get(`/api/${url}`, {
+    headers: { Authorization: token }
+  });
+  return res;
 };
 
 /**
@@ -22,10 +28,10 @@ export const getDataAPI = async (url, token) => {
  * @returns {Promise} - Axios response promise
  */
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`/api/${url}`, post, {
-        headers: { Authorization: token }
-    });
-    return res;
+  const res = await API.post(`/api/${url}`, post, {
+    headers: { Authorization: token }
+  });
+  return res;
 };
 
 /**
@@ -36,10 +42,10 @@ export const postDataAPI = async (url, post, token) => {
  * @returns {Promise} - Axios response promise
  */
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`/api/${url}`, post, {
-        headers: { Authorization: token }
-    });
-    return res;
+  const res = await API.put(`/api/${url}`, post, {
+    headers: { Authorization: token }
+  });
+  return res;
 };
 
 /**
@@ -50,10 +56,10 @@ export const putDataAPI = async (url, post, token) => {
  * @returns {Promise} - Axios response promise
  */
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`/api/${url}`, post, {
-        headers: { Authorization: token }
-    });
-    return res;
+  const res = await API.patch(`/api/${url}`, post, {
+    headers: { Authorization: token }
+  });
+  return res;
 };
 
 /**
@@ -63,8 +69,8 @@ export const patchDataAPI = async (url, post, token) => {
  * @returns {Promise} - Axios response promise
  */
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
-        headers: { Authorization: token }
-    });
-    return res;
+  const res = await API.delete(`/api/${url}`, {
+    headers: { Authorization: token }
+  });
+  return res;
 };
